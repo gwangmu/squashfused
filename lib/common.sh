192c5d1d3b11f6d59cb3f40d53d4e17d1b368e81
Gwangmu Lee <gwangmu.lee@cscs.ch>
2026-07-30 22:01:58 +0200

    polishing

---
+++ lib/common.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+log::file() {
+  local filepath=$1
+  exec 3>"$filepath"
+}
+
+log::echo() {
+  local msg=${1:-}
+  echo "$msg" >&3 || true
+}
+
+state::load() {  
+  CONTAINER_STATE=$(cat)
+}
+
+state::read() {
+  local field=$1
+  echo "$CONTAINER_STATE" | jq "$field"
+}
+
+state::add() {
+  local field=$1
+  local value=$2
+  CONTAINER_STATE=$(echo "$CONTAINER_STATE" | jq "$field"' += '"$value")
+}
+
+state::print() {
+  echo "$CONTAINER_STATE"
+}
