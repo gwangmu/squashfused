c394f3f29774d92257f8fb2b71426590f8daf7d8
Gwangmu Lee <gwangmu.lee@cscs.ch>
2026-08-10 16:12:17 +0200

    Support multi-users

--- lib/path.sh
+++ lib/path.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-SERVER_DIR_PATH="/tmp/squashfused-server"
-SERVER_FIFO_PATH="$SERVER_DIR_PATH/spawn"
-SERVER_MAP_DIR_PATH="$SERVER_DIR_PATH/map"
-SERVER_LOG_DIR_PATH="$SERVER_DIR_PATH/log"
-
+SERVER_DIR_PATH_PREFIX="/tmp/squashfused-server-"
 DAEMON_DIR_HOST_PATH_PREFIX="/tmp/squashfused-"
+
 DAEMON_DIR_CONT_PATH="/squashfused"
 BRIDGE_DIR_CONT_PATH="$DAEMON_DIR_CONT_PATH/bridge_dir"
 BRIDGE_FIFO_CONT_PATH="$DAEMON_DIR_CONT_PATH/bridge_fifo"
+
+path::set_server() {
+  local HOST_ID=${1:-?}
+  if [[ "$HOST_ID" == "?" ]]; then
+    # Find a server that this can talk to.
+    # This is a workaround to identifying the UID from hooks, which is a pain.
+    for CANDIDATE_DIR in $(ls $(dirname "$SERVER_DIR_PATH_PREFIX") | grep $(basename "$SERVER_DIR_PATH_PREFIX")); do
+      CANDIDATE_PATH=$(dirname "$SERVER_DIR_PATH_PREFIX")/"$CANDIDATE_DIR"
+      if [[ -w "$CANDIDATE_PATH/spawn" ]]; then
+        HOST_ID=${CANDIDATE_PATH#$SERVER_DIR_PATH_PREFIX}
+      fi
+    done
+  fi
+  export SERVER_DIR_PATH="$SERVER_DIR_PATH_PREFIX""$HOST_ID"
+  export SERVER_FIFO_PATH="$SERVER_DIR_PATH/spawn"
+  export SERVER_MAP_DIR_PATH="$SERVER_DIR_PATH/map"
+  export SERVER_LOG_DIR_PATH="$SERVER_DIR_PATH/log"
+}
 
 path::set_host() {
   local DAEMON_ID=$1
